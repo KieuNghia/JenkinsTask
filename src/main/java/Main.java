d88a7561a7d9b34acbf50e23e577046270b0346c
jekaufo <jekaufo@gmail.com>
2015-10-11 14:13:50 +0300

    minor update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.util.Calendar;
 
 public class Main {
 
@@ -11,11 +10,9 @@ public class Main {
 
         URL website = new URL("https://dl.dropboxusercontent.com/u/98396761/NewTxt.txt");
         ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-        Calendar calendar = Calendar.getInstance();
-        String time = ""+  calendar.get(Calendar.HOUR) +":"+ calendar.get(Calendar.MINUTE)+"";
-        FileOutputStream fos = new FileOutputStream("File" +time+".txt");
+        FileOutputStream fos = new FileOutputStream("File.txt");
         fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-        System.out.println("File "+time+"  created");
+        System.out.println("File created");
     }
 }
 
